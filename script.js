a44ff4b7fce0a8d191c9750380e1aa96cab2d79c
Robert Anthony <rwanthony3@gmail.com>
2022-03-04 15:31:25 -0800

    updated button styling and page content

---
+++ script.js
@@ -0,0 +1,28 @@
+var question = document.querySelector("#question");
+var startTime = 60;
+
+var questionIndex = 0;
+questionBank[questionIndex].question;
+
+var questionBank = [
+    {
+    question:"What is the first question?",
+    possibleAnswers: ["A", "B", "C", "D"],
+    correctAnswer: "A",
+},
+{
+    question:"What is the second question?",
+    possibleAnswers: ["A", "B", "C", "D"],
+    correctAnswer: "B",
+},
+{
+    question:"What is the third question?",
+    possibleAnswers: ["A", "B", "C", "D"],
+    correctAnswer: "C",
+}, 
+{
+    question:"What is the forth question?",
+    possibleAnswers: ["A", "B", "C", "D"],
+    correctAnswer: "D",
+}]
+
