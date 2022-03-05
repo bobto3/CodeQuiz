c5f64dc2a3829fee7b32cbe8cb1093e8ddc8efbd
Robert Anthony <rwanthony3@gmail.com>
2022-03-04 17:16:28 -0800

    start game button functionality

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
-var question = document.querySelector("#question");
-var startTime = 60;
+var questionEl = document.querySelector("#question");
+var questionBankEl = document.querySelector("#questionBank");
+var questionCardEl = document.querySelector("#questionCard");
+var startGameButtonEl = document.querySelector("#startGameButton");
+var welcomeScreenEl = document.querySelector("#welcomeScreen")
+var timerScoreEl = document.querySelector("#timerScore");
 
 var questionIndex = 0;
-questionBank[questionIndex].question;
+//questionBank[questionIndex].question;
 
 var questionBank = [
     {
@@ -26,3 +30,28 @@ var questionBank = [
     correctAnswer: "D",
 }]
 
+function startGame() {
+    questionCardEl.style.display = "flex";
+    welcomeScreenEl.style.display = "none";
+
+}
+
+var startTime = 5;
+
+function startTimer() {
+    var timerInterval = setInterval(function() {
+        timerScoreEl.textContent = "Time: " + startTime + " seconds left";
+        startTime--;
+
+        if(startTime === -1) {
+            // Stops execution of action at set interval
+            clearInterval(timerInterval);
+        }
+    }, 1000);
+}
+
+startGameButtonEl.addEventListener("click", function() {
+    startGame();
+    startTimer();
+    console.log("start game");
+})
